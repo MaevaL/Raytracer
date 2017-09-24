d48d13c5a357583dbc75651f32bffb0272dbade4
MaevaL <maeva.lauzier@gmail.com>
2017-09-25 00:56:41 +0200

    Raytracer multi mirror done

---
+++ Raytracer/src/models/Ray.java
@@ -0,0 +1,36 @@
+package models;
+
+public class Ray {
+	private Vec3 origine = new Vec3(0,0,0);
+	private Vec3 direction = new Vec3(0,0,0);
+	
+	public Ray(Vec3 r1, Vec3 r2){
+		origine.setCoordinate(r1.getX(),r1.getY(), r1.getZ());
+		direction.setCoordinate(r2.getX(),r2.getY(), r2.getZ());
+		direction = direction.normalized();
+}
+	
+	public Vec3 getOrigine() {
+		return origine;
+	}
+
+	public void setOrigine(Vec3 origine) {
+		this.origine = origine;
+	}
+
+	public Vec3 getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Vec3 direction) {
+		this.direction = direction;
+	}
+	
+	public Vec3 position3D(double t) {
+		Vec3 r = origine.sum(direction.multiplyByNumber(t));
+		return r;
+	}
+
+	
+
+}
---
+++ Raytracer/src/models/Sphere.java
@@ -0,0 +1,54 @@
+package models;
+
+public class Sphere {
+	private Vec3 origin = new Vec3(0,0,0);
+	private Vec3 color = new Vec3(0,0,0);
+	private double raySphere = 0;
+	private Boolean is_mirror = false;
+	
+	public Sphere(Vec3 origin, Vec3 color, double raySphere, Boolean is_mirror) {
+		this.origin = origin;
+		this.color = color;
+		this.raySphere = raySphere;
+		this.is_mirror = is_mirror;
+	}
+	
+	public Vec3 getColor() {
+		return color;
+	}
+
+	public void setColor(Vec3 color) {
+		this.color = color;
+	}
+
+	public Boolean getIs_mirror() {
+		return is_mirror;
+	}
+
+	public void setIs_mirror(Boolean is_mirror) {
+		this.is_mirror = is_mirror;
+	}
+
+
+	public Vec3 getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(Vec3 origin) {
+		this.origin = origin;
+	}
+
+	public double getRaySphere() {
+		return raySphere;
+	}
+
+	public void setRaySphere(double raySphere) {
+		this.raySphere = raySphere;
+	}
+	
+
+	
+	
+	
+
+}
